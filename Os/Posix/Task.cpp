fe44721ab5d10b7b81a908eebb6c6cb606140b4f
Ryan Curtin <ryan@ratml.org>
2026-01-07 13:59:04 -0500

    Debugging output for creating tasks.

--- Os/Posix/Task.cpp
+++ Os/Posix/Task.cpp
@@ -7,6 +7,7 @@
 #include <cerrno>
 #include <climits>
 #include <cstring>
+#include <cstdio>
 
 #include "Fw/Logger/Logger.hpp"
 #include "Fw/Types/Assert.hpp"
@@ -27,6 +28,69 @@ void* pthread_entry_wrapper(void* wrapper_pointer) {
     Os::Task::TaskRoutineWrapper& wrapper = *reinterpret_cast<Os::Task::TaskRoutineWrapper*>(wrapper_pointer);
     wrapper.run(&wrapper);
     return nullptr;
+}
+
+static void handle_error_en(int s, const char* name)
+{
+  printf("Error %d: %s\n", s, name);
+}
+
+static void
+display_pthread_attr(pthread_attr_t *attr)
+{
+    int s, i;
+    size_t v;
+    void *stkaddr;
+    struct sched_param sp;
+
+    s = pthread_attr_getdetachstate(attr, &i);
+    if (s != 0)
+        handle_error_en(s, "pthread_attr_getdetachstate");
+    printf("Detach state        = %s\n",
+            (i == PTHREAD_CREATE_DETACHED) ? "PTHREAD_CREATE_DETACHED" :
+            (i == PTHREAD_CREATE_JOINABLE) ? "PTHREAD_CREATE_JOINABLE" :
+            "???");
+
+    s = pthread_attr_getscope(attr, &i);
+    if (s != 0)
+        handle_error_en(s, "pthread_attr_getscope");
+    printf("Scope               = %s\n",
+            (i == PTHREAD_SCOPE_SYSTEM)  ? "PTHREAD_SCOPE_SYSTEM" :
+            (i == PTHREAD_SCOPE_PROCESS) ? "PTHREAD_SCOPE_PROCESS" :
+            "???");
+
+    s = pthread_attr_getinheritsched(attr, &i);
+    if (s != 0)
+        handle_error_en(s, "pthread_attr_getinheritsched");
+    printf("Inherit scheduler   = %s\n",
+            (i == PTHREAD_INHERIT_SCHED)  ? "PTHREAD_INHERIT_SCHED" :
+            (i == PTHREAD_EXPLICIT_SCHED) ? "PTHREAD_EXPLICIT_SCHED" :
+            "???");
+
+    s = pthread_attr_getschedpolicy(attr, &i);
+    if (s != 0)
+        handle_error_en(s, "pthread_attr_getschedpolicy");
+    printf("Scheduling policy   = %s\n",
+            (i == SCHED_OTHER) ? "SCHED_OTHER" :
+            (i == SCHED_FIFO)  ? "SCHED_FIFO" :
+            (i == SCHED_RR)    ? "SCHED_RR" :
+            "???");
+
+    s = pthread_attr_getschedparam(attr, &sp);
+    if (s != 0)
+        handle_error_en(s, "pthread_attr_getschedparam");
+    printf("Scheduling priority = %d\n", sp.sched_priority);
+
+    s = pthread_attr_getguardsize(attr, &v);
+    if (s != 0)
+        handle_error_en(s, "pthread_attr_getguardsize");
+    printf("Guard size          = %d bytes\n", v);
+
+    s = pthread_attr_getstack(attr, &stkaddr, &v);
+    if (s != 0)
+        handle_error_en(s, "pthread_attr_getstack");
+    printf("Stack address       = %p\n", stkaddr);
+    printf("Stack size          = 0x%x bytes\n", v);
 }
 
 int set_stack_size(pthread_attr_t& attributes, const Os::Task::Arguments& arguments) {
@@ -136,6 +200,7 @@ Os::Task::Status PosixTask::create(const Os::Task::Arguments& arguments,
         (pthread_status == PosixTaskHandle::SUCCESS)) {
         pthread_status = set_cpu_affinity(attributes, arguments);
     }
+    display_pthread_attr(&attributes);
     if (pthread_status == PosixTaskHandle::SUCCESS) {
         pthread_status =
             pthread_create(&handle.m_task_descriptor, &attributes, pthread_entry_wrapper, arguments.m_routine_argument);
